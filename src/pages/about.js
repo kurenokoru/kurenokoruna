a7244103a572b515ec2515abad903e079e35a374
kurenokoru <laaajosie@gmail.com>
2019-08-14 18:31:11 +0200

    apres avoir ajouté les plugin du default starter

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "gatsby"
+
+//import Layout from '../components/layout'
+
+const AboutPage = () => (
+<div>
+        <h1>About</h1>
+        <p>salut, c'est moi c'est moi nana!</p>
+        <Link to ="/">home</Link>
+</div>
+)
+
+export default AboutPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,14 @@
-import React from "react"
+import React from 'react'
+import { Link } from 'gatsby'
 
-export default () => <div>Hello world!</div>
+import Layout from '../components/layout'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>INDEX</h1>
+    <p>Hey! This is ALL ABOUT ME!!!</p>
+    <Link to="/about/">About</Link>
+  </Layout>
+)
+
+export default IndexPage
